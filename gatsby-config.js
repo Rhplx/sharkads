60e32b7298d490c05f39e3e1123d715287c3df93
Luis <luis@indg.dev>
2021-07-28 00:37:50 -0500

    FIX: Gatsby pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/sharkads",
+  pathPrefix: "/",
   siteMetadata: {
     title: `Sharkads`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
